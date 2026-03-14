a42b4f9d9a8f35703271201368fb41f32b82173b
Phellipe Perin <phellipe.perin@gmail.com>
2026-03-14 08:47:26 +0100

    refactor: use of startsWith given naming assumption

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,16 +19,19 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (
+        !item.name.startsWith('Aged Brie') &&
+        !item.name.startsWith('Backstage passes')
+      ) {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          if (!item.name.startsWith('Sulfuras')) {
             item.quality = item.quality - 1
           }
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.name.startsWith('Backstage passes')) {
             if (item.sellIn < 11) {
               if (item.quality < 50) {
                 item.quality = item.quality + 1
@@ -42,14 +45,14 @@ export class GildedRose {
           }
         }
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (!item.name.startsWith('Sulfuras')) {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (!item.name.startsWith('Aged Brie')) {
+          if (!item.name.startsWith('Backstage passes')) {
             if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              if (!item.name.startsWith('Sulfuras')) {
                 item.quality = item.quality - 1
               }
             }
