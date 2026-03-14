ec0f10156f60ed62f31e7ea878f91c3747ede1a9
Phellipe Perin <phellipe.perin@gmail.com>
2026-03-14 08:51:40 +0100

    refactor: sulfuras item handling

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,14 +19,16 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
+      if (item.name.startsWith('Sulfuras')) {
+        continue;
+      }
+
       if (
         !item.name.startsWith('Aged Brie') &&
         !item.name.startsWith('Backstage passes')
       ) {
         if (item.quality > 0) {
-          if (!item.name.startsWith('Sulfuras')) {
-            item.quality = item.quality - 1
-          }
+          item.quality = item.quality - 1
         }
       } else {
         if (item.quality < 50) {
@@ -45,16 +47,11 @@ export class GildedRose {
           }
         }
       }
-      if (!item.name.startsWith('Sulfuras')) {
-        item.sellIn = item.sellIn - 1;
-      }
       if (item.sellIn < 0) {
         if (!item.name.startsWith('Aged Brie')) {
           if (!item.name.startsWith('Backstage passes')) {
             if (item.quality > 0) {
-              if (!item.name.startsWith('Sulfuras')) {
-                item.quality = item.quality - 1
-              }
+              item.quality = item.quality - 1
             }
           } else {
             item.quality = item.quality - item.quality
