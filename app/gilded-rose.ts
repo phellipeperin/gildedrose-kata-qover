cf57d2fd9642bfb5703ad604dfa98625cb8b244b
Phellipe Perin <phellipe.perin@gmail.com>
2026-03-14 09:05:56 +0100

    refactor: aged brie item handling

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,10 +23,18 @@ export class GildedRose {
         continue;
       }
 
-      if (
-        !item.name.startsWith('Aged Brie') &&
-        !item.name.startsWith('Backstage passes')
-      ) {
+      if (item.name.startsWith('Aged Brie')) {
+        item.sellIn = item.sellIn - 1;
+        if (item.quality < 50) {
+          item.quality += item.sellIn < 0 ? 2 : 1;
+        }
+        if (item.quality > 50) {
+          item.quality = 50;
+        }
+        continue;
+      }
+
+      if (!item.name.startsWith('Backstage passes')) {
         if (item.quality > 0) {
           item.quality = item.quality - 1
         }
@@ -48,18 +56,15 @@ export class GildedRose {
         }
       }
       if (item.sellIn < 0) {
-        if (!item.name.startsWith('Aged Brie')) {
-          if (!item.name.startsWith('Backstage passes')) {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1
-            }
-          } else {
-            item.quality = item.quality - item.quality
+        if (!item.name.startsWith('Backstage passes')) {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+          item.quality = item.quality - item.quality
+        }
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
         }
       }
     }
